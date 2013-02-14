08b79c771b683c128fd391c32c8c33c946f7399a
Smit Shah <who828@gmail.com>
2013-02-14 10:00:10 +0530

    Initial commit

---
+++ parsing/lexer/src/Token.java
@@ -0,0 +1,17 @@
+public class Token {
+    public int type;
+    public String text;
+
+    public Token(int type, String text)
+    {
+        this.type = type;
+        this.text = text;
+    }
+
+    public  String toString()
+    {
+        String name = ListLexer.tokenNames[type];
+        return "<'" +text+ "'," +tname+">";
+    }
+
+}
